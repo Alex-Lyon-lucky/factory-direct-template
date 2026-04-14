e58d3feb26132c4871e9bbeb4f081e76feacfec8
Alex-Lyon-lucky <l17631687547@gmail.com>
2026-04-14 11:07:40 +0800

    Fix missing Link import in ProductForm

--- app/admin/products/ProductForm.tsx
+++ app/admin/products/ProductForm.tsx
@@ -3,6 +3,7 @@
 
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useProducts, Product } from '../../context/ProductContext';
 
